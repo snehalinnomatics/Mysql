32795aa85815586890dbdfc504d72ab9e3f2e999
snehalinnomatics <snehalinnomatics@gmail.com>
2026-01-28 19:34:30 +0530

    Add files via upload

---
+++ trains_learndb table.sql
@@ -0,0 +1,10 @@
+use learndb;
+
+create table trains(Train_id integer,   Station varchar(50),  Time time);
+insert into trains values (110,'San Francisco', '10:00:00'),
+(110,'Redwood City',  '10:54:00'),
+(110,'Palo Alto','11:02:00'),
+(110 ,'San Jose', '12:35:00'),
+(120,'San Francisco','11:00:00'),
+(120 ,'Palo Alto', '12:49:00'),
+(120,'San Jose', '13:30:00');
